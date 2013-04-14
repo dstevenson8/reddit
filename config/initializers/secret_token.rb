fc5cd4bb89ef79f7b090695c70bfd5dc2014c5dd
Ingmaras Keleras <as@ingmaras.com>
2013-04-14 21:19:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddit::Application.config.secret_token = '3b483598a47bbc187353f2b099dac2e1c590e37c46714c470a27b759c8b88cf222160a623bb5a3238015cf89a9e9c10fd6b330ade3623571b3ecaa8d4dc00c47'
