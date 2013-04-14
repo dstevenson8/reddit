f38a76d60e1df0614e57542212a25e93172c1041
Ingmaras Keleras <as@ingmaras.com>
2013-04-14 23:58:53 +0100

    Fix votes_up and votes_down migration (default, not null)

--- db/migrate/20130414225537_add_votes_up_and_votes_down_to_users.rb
+++ db/migrate/20130414225537_add_votes_up_and_votes_down_to_users.rb
@@ -1,6 +1,6 @@
 class AddVotesUpAndVotesDownToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :votes_up, :integer
-    add_column :users, :votes_down, :integer
+    add_column :users, :votes_up, :integer, default: 0, null: false
+    add_column :users, :votes_down, :integer, default: 0, null: false
   end
 end
